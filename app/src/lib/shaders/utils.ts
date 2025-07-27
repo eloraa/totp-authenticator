24815a22e1bc26687260286cad778a2cc72c1d63
elora <56364111+eloraa@users.noreply.github.com>
2025-07-27 11:31:39 +0100

    add missing shader utils

--- app/src/lib/shaders/utils.ts
+++ app/src/lib/shaders/utils.ts
@@ -109,3 +109,41 @@ float grainShape(vec2 uv, vec2 seedOffset) {
   return 10. * u_scale * length(vec2(dFdx(total), dFdy(total)));
 }
 `;
+
+interface PropsWithColors {
+  colors?: string[];
+  [key: string]: unknown;
+}
+
+export function colorPropsAreEqual(prevProps: PropsWithColors, nextProps: PropsWithColors): boolean {
+  for (const key in prevProps) {
+    if (key === 'colors') {
+      const prevIsArray = Array.isArray(prevProps.colors);
+      const nextIsArray = Array.isArray(nextProps.colors);
+
+      if (!prevIsArray || !nextIsArray) {
+        if (Object.is(prevProps.colors, nextProps.colors) === false) {
+          return false;
+        }
+
+        continue;
+      }
+
+      if (prevProps.colors?.length !== nextProps.colors?.length) {
+        return false;
+      }
+
+      if (!prevProps.colors?.every((color, index) => color === nextProps.colors?.[index])) {
+        return false;
+      }
+
+      continue;
+    }
+
+    if (Object.is(prevProps[key], nextProps[key]) === false) {
+      return false;
+    }
+  }
+
+  return true;
+}
